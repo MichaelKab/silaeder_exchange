f1825c02f78541c0f657f5d3e364c2e10d8afc75
MichaelKab <michael.kabanov4@gmail.com>
2021-01-26 19:04:22 +0300

    sm_c 3

--- silaeder_exchange/settings.py
+++ silaeder_exchange/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
-    "rest_framework",
+    #"rest_framework",
 ]
 
 MIDDLEWARE = [

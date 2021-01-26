e28bfc1d9cb5051daa634b9dd4bbad8ec82bb3ab
MichaelKab <michael.kabanov4@gmail.com>
2021-01-26 19:59:49 +0300

    sm_c 4

--- silaeder_exchange/settings.py
+++ silaeder_exchange/settings.py
@@ -40,7 +40,13 @@ INSTALLED_APPS = [
     "rest_framework",
     'website.apps.WebsiteConfig',
 ]
-
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
     #"whitenoise.storage.CompressedManifestStaticFilesStorage",

3c11357607ba072a4536ea63f62a7bd31bff35a5
MichaelKab <michael.kabanov4@gmail.com>
2021-04-24 11:42:00 +0300

    secr and more pretty

--- silaeder_exchange/settings.py
+++ silaeder_exchange/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import dj_database_url
+from .secr import SECRET_K
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+z36ql4cwd@x+e4l_jk$+6k(uztww^7p4s49asq53b-7q0tmu9'
+SECRET_KEY = SECRET_K
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
